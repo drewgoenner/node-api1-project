a131cd712c95b869eaca7974a9ae96ffe3aab6cd
drewgoenner <52584951+drewgoenner@users.noreply.github.com>
2019-09-30 15:10:00 -0500

    created post for adding a new user

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // implement your API here
+const express = require('express');
+
+const db = require('./data/db.js');
+
+const server = express();
+
+server.use(express.json());
+
+server.post('/api/users', (req, res) => {
+
+    const userData = req.body;
+
+    if(!userData.name || !userData.bio) {
+        res.status(400).json({errorMessage: "Please provide name and bio for user"});
+    } else {
+        db
+        .insert(userData)
+        .then(user => {
+            res.status(201).json(user);
+        })
+        .catch(err => {
+            res.status(500).json({error: "There was an error while saving the user to the database"})
+        })
+    }
+    
+})
+
+const port = 8000
+server.listen(port, () => console.log(`\n** API on port ${port} **\n`))
