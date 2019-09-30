a361dd35ae7fd46e009cf1666eac6ecfcb3fc730
drewgoenner <52584951+drewgoenner@users.noreply.github.com>
2019-09-30 16:25:42 -0500

    completed all except for put (which works, but throws an unhandled rejection error even though it does)

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // implement your API here
 const express = require('express');
 
-const db = require('./data/db.js');
+const userDb = require('./data/db.js');
 
 const server = express();
 
@@ -14,7 +14,7 @@ server.post('/api/users', (req, res) => {
     if(!userData.name || !userData.bio) {
         res.status(400).json({errorMessage: "Please provide name and bio for user"});
     } else {
-        db
+        userDb
         .insert(userData)
         .then(user => {
             res.status(201).json(user);
@@ -24,6 +24,72 @@ server.post('/api/users', (req, res) => {
         })
     }
     
+})
+
+server.get('/api/users', (req, res) => {
+    userDb.find()
+    .then(users => {
+        res.json(users);
+    })
+    .catch(err => {
+        res.status(500).json({error: "The users information could not be retrieved"})
+    })
+})
+
+server.get('/api/users/:id', (req, res) => {
+    const id = req.params.id
+        
+    userDb.findById(id)
+    .then(user => {
+        if(user) {
+            res.json(user)
+        } else{
+            res.status(404).json({ message: "The user with the specified ID does not exist"})
+        }
+    })
+    .catch(err => {
+        res.status(500).json({ error: "The user information could not be retrieved"})
+    })
+})
+
+server.delete('/api/users/:id', (req, res) => {
+    const id = req.params.id;
+   
+    userDb.remove(id)
+    .then(user => {
+        if(user) {
+            res.json({message: "User has been removed from the database"})
+        } else{
+            res.status(404).json({message: "The user with the specified ID does not exist"})
+        }
+    })
+    .catch(err => {
+        res.status(500).json({ error: "The user could not be removed"})
+    })
+})
+
+server.put('/api/users/:id', (req, res) => {
+    const id = req.params.id;
+    const updatedUser = req.body
+    if(!updatedUser.name || !updatedUser.bio) {
+        res.status(400).json({errorMessage: "Please provide name and bio for user"});
+    } else {
+    userDb.update(id, updatedUser)
+        .then(user => {
+            if(user) {
+                userDb.findById(id)
+                    .then(upUser => {
+                        res.json(upUser)
+                    })
+            } else {
+                res.status(404).json({ message: "The user with the specified ID does not exist"})
+            }
+        })
+        .then(err => {
+            res.status(500).json({ error: "The user information could not be modified"})
+        })
+    }
+
 })
 
 const port = 8000
